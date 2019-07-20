ce1d55a588e09682cec4bf5d93939247b9028b6d
Yaofeng Zhong <yaofeng.zhong@siemens.com>
2019-07-20 19:18:40 -0400

    training loop WIP

---
+++ experiment-single-force/data.py
@@ -0,0 +1,157 @@
+# code borrowed from Sam Greydanus
+# https://github.com/greydanus/hamiltonian-nn
+
+import autograd
+import autograd.numpy as np
+
+import scipy.integrate
+solve_ivp = scipy.integrate.solve_ivp
+from utils import to_pickle, from_pickle
+import gym
+
+def hamiltonian_fn(coords):
+    q, p = np.split(coords,2)
+    # pendulum hamiltonian conosistent with openAI gym Pendulum-v0
+    H = 5*(1-np.cos(q)) + 1.5 * p**2 
+    return H
+
+def dynamics_fn(t, coords, u):
+    dcoords = autograd.grad(hamiltonian_fn)(coords)
+    dqdt, dpdt = np.split(dcoords,2)
+    S = np.concatenate([dpdt, -dqdt + u], axis=-1)
+    return S
+
+def get_trajectory(t_span=[0,3], timescale=20, radius=None, y0=None, noise_std=0.1, u=0.0, **kwargs):
+    t_eval = np.linspace(t_span[0], t_span[1], int(timescale*(t_span[1]-t_span[0])))
+    
+    # # get initial state
+    # if y0 is None:
+    #     y0 = np.random.rand(2)*2.-1
+    # if radius is None:
+    #     radius = np.random.rand() + 1.3 # sample a range of radii
+    # y0 = y0 / np.sqrt((y0**2).sum()) * radius ## set the appropriate radius
+    
+    spring_ivp = solve_ivp(lambda t, y: dynamics_fn(t, y, u), t_span=t_span, y0=y0, t_eval=t_eval, rtol=1e-10, **kwargs)
+    q, p = spring_ivp['y'][0], spring_ivp['y'][1]
+    # dydt = [dynamics_fn(None, y) for y in spring_ivp['y'].T]
+    # dydt = np.stack(dydt).T
+    # dqdt, dpdt = np.split(dydt,2)
+
+    # add noise
+    q += np.random.randn(*q.shape)*noise_std
+    p += np.random.randn(*p.shape)*noise_std
+
+    return q, p, t_eval
+
+def get_theta(cos, sin):
+    theta = np.arctan2(sin, cos)
+    theta = theta + 2*np.pi if theta < -np.pi else theta
+    theta = theta - 2*np.pi if theta > np.pi else theta
+    return theta
+
+
+def get_q_p(obs):
+    '''construct q and p from gym observations of Pendulum-v0'''
+    x = - 0.5 * obs[1]
+    y = 0.5 * obs[0]
+    theta = get_theta(-obs[0], -obs[1])
+    p_x = - 0.5 * obs[0] * obs[2]
+    p_y = - 0.5 * obs[1] * obs[2]
+    p_theta = obs[2] / 12.0  # p_theta in the COM
+    return np.array([x, y, theta, p_x, p_y, p_theta])
+
+
+def sample_gym(seed=0, timesteps=103, trials=200, side=28, min_angle=0., max_angle=np.pi/6, 
+              verbose=False, env_name='Pendulum-v0'):
+    
+    gym_settings = locals()
+    if verbose:
+        print("Making a dataset of Pendulum observations.")
+    env: gym.wrappers.time_limit.TimeLimit = gym.make(env_name)
+    env.reset() ; env.seed(seed)
+
+    trajs = []
+    for trial in range(trials):
+        traj = []
+        for step in range(timesteps):
+
+            if step == 0:
+                angle_ok = False
+
+                while not angle_ok:
+                    obs = env.reset()
+                    theta_init = np.abs(get_theta(-obs[0], -obs[1]))
+                    if verbose:
+                        print("\tCalled reset. Max angle= {:.3f}".format(theta_init))
+                    if theta_init > min_angle and theta_init < max_angle:
+                        angle_ok = True
+                if verbose:
+                    print("\tRunning environment...")
+            
+            obs, _, _, _ = env.step([0.0]) # no action
+            x = get_q_p(obs)
+            traj.append(x)
+        traj = np.stack(traj)
+        trajs.append(traj)
+    trajs = np.stack(trajs) # (trials, timesteps, 6)
+    trajs = np.transpose(trajs, (1, 0, 2)) # (timesteps, trails, 6)
+    tspan = np.arange(timesteps) * 0.05
+    return trajs, tspan, gym_settings
+
+def get_dataset(seed=0, samples=50, test_split=0.5, gym=False, save_dir=None, us=[0], **kwargs):
+    data = {}
+
+    if gym:
+        assert save_dir is not None
+        path = '{}/acrobot-gym-dataset.pkl'.format(save_dir)
+        try:
+            data = from_pickle(path)
+            print("Successfully loaded data from {}".format(path))
+        except:
+            print("Had a problem loading data from {}. Rebuilding dataset...".format(path))
+
+            trajs, tspan, _ = sample_gym(seed=seed, trials=samples, **kwargs)
+            split_ix = int(trajs.shape[1]*test_split)
+            data['x'], data['test_x'] = trajs[:, split_ix:, :], trajs[:, :split_ix, :]
+            data['t'] = tspan
+
+            to_pickle(data, path)
+    else:
+        # randomly sample inputs
+        np.random.seed(seed)
+        xs_force = []
+        for u in us:
+            xs = []
+            for _ in range(samples):
+                q, p, t = get_trajectory(noise_std=0.0, u=u, **kwargs)
+                xs.append(np.stack((q, p, np.ones_like(q)*u), axis=1)) # (45, 3) last dimension is u
+            xs_force.append(np.stack(xs, axis=1)) # fit Neural ODE format (45, 50, 3)
+            
+            
+        data['x'] = np.stack(xs_force, axis=0) # (3, 45, 50, 3)
+
+
+        # make a train/test split
+        split_ix = int(samples * test_split)
+        split_data = {}
+        split_data['x'], split_data['test_x'] = data['x'][:,:,:split_ix,:], data['x'][:,:,split_ix:,:]
+
+        data = split_data
+        data['t'] = t
+    
+    return data
+
+def arrange_data(x, t, num_points=2):
+    '''Arrange data to feed into neural ODE in small chunks'''
+    assert num_points>=2 and num_points<=len(t)
+    x_stack = []
+    for i in range(num_points):
+        if i < num_points-1:
+            x_stack.append(x[:, i:-num_points+i+1,:,:])
+        else:
+            x_stack.append(x[:, i:,:,:])
+    x_stack = np.stack(x_stack, axis=1)
+    x_stack = np.reshape(x_stack, 
+                (x.shape[0], num_points, -1, x.shape[3]))
+    t_eval = t[0:num_points]
+    return x_stack, t_eval
---
+++ experiment-single-force/train.py
@@ -0,0 +1,100 @@
+# code borrowed from Sam Greydanus
+# https://github.com/greydanus/hamiltonian-nn
+
+import torch, argparse
+import numpy as np
+
+import os, sys
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(PARENT_DIR)
+
+from nn_models import MLP, PSD, ConstraintNet
+from hnn import HNN_structure_forcing
+from data import get_dataset, arrange_data
+from utils import L2_loss, to_pickle
+
+import time
+
+def get_args():
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('--input_dim', default=3, type=int, help='dimensionality of input tensor')
+    parser.add_argument('--hidden_dim', default=600, type=int, help='hidden dimension of mlp')
+    parser.add_argument('--learn_rate', default=1e-3, type=float, help='learning rate')
+    parser.add_argument('--nonlinearity', default='tanh', type=str, help='neural net nonlinearity')
+    parser.add_argument('--total_steps', default=2000, type=int, help='number of gradient steps')
+    parser.add_argument('--print_every', default=200, type=int, help='number of gradient steps between prints')
+    parser.add_argument('--name', default='pend', type=str, help='only one option right now')
+    parser.add_argument('--baseline', dest='baseline', action='store_true', help='run baseline or experiment?')
+    parser.add_argument('--verbose', dest='verbose', action='store_true', help='verbose?')
+    parser.add_argument('--seed', default=0, type=int, help='random seed')
+    parser.add_argument('--save_dir', default=THIS_DIR, type=str, help='where to save the trained model')
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--num_points', type=int, default=2, help='number of evaluation points by the ODE solver, including the initial point')
+    parser.add_argument('--structure', dest='structure', action='store_true', help='using a structured Hamiltonian')
+    parser.add_argument('--gym', dest='gym', action='store_true', help='use OpenAI gym to generate data')
+    parser.set_defaults(feature=True)
+    return parser.parse_args()
+
+def train(args):
+    # import ODENet
+    from torchdiffeq import odeint
+
+    device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+
+    # reproducibility: set random seed
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    # init model and optimizer
+    if args.verbose:
+        print("Training baseline ODE model with num of points = {}:".format(args.num_points) if args.baseline 
+            else "Training HNN ODE model with num of points = {}:".format(args.num_points))
+        if args.structure:
+            print("using the structured Hamiltonian")
+    
+    if args.structure == False:
+        output_dim = args.input_dim if args.baseline else 1
+        nn_model = MLP(args.input_dim, args.hidden_dim, output_dim, args.nonlinearity).to(device)
+        g_net = MLP(int(args.input_dim/3), args.hidden_dim, int(args.input_dim/3)).to(device)
+        model = HNN_structure_forcing(args.input_dim, H_net=nn_model, g_net=g_net, device=device, baseline=args.baseline)
+    elif args.structure == True and args.baseline ==False:
+    
+        M_net = MLP(1, args.hidden_dim, 1).to(device)
+        V_net = MLP(int(args.input_dim/3), 4, 1).to(device)
+        g_net = MLP(int(args.input_dim/3), args.hidden_dim, int(args.input_dim/3)).to(device)
+        model = HNN_structure_forcing(args.input_dim, M_net=M_net, V_net=V_net, G_net=F_net, device=device, baseline=args.baseline, structure=True).to(device)
+    else:
+        raise RuntimeError('argument *baseline* and *structure* cannot both be true')
+
+
+    optim = torch.optim.Adam(model.parameters(), args.learn_rate, weight_decay=1e-4)
+
+    # arrange data
+    data = get_dataset(seed=args.seed, gym=args.gym, save_dir=args.save_dir)
+    train_x, t_eval = arrange_data(data['x'], data['t'], num_points=args.num_points)
+    test_x, t_eval = arrange_data(data['test_x'], data['t'], num_points=args.num_points)
+
+    train_x = torch.tensor(train_x, requires_grad=True, dtype=torch.float32).to(device) # (45, 25, 2)
+    test_x = torch.tensor(test_x, requires_grad=True, dtype=torch.float32).to(device)
+    t_eval = torch.tensor(t_eval, requires_grad=True, dtype=torch.float32).to(device)
+
+    # training loop
+    
+
+
+if __name__ == "__main__":
+    args = get_args()
+    model, stats = train(args)
+
+    # save 
+    os.makedirs(args.save_dir) if not os.path.exists(args.save_dir) else None
+    label = '-baseline_ode' if args.baseline else '-hnn_ode'
+    struct = '-struct' if args.structure else ''
+    path = '{}/{}{}{}-p{}.tar'.format(args.save_dir, args.name, label, struct, args.num_points)
+    torch.save(model.state_dict(), path)
+
+    path = '{}/{}{}{}-p{}-stats.pkl'.format(args.save_dir, args.name, label, struct, args.num_points)
+    to_pickle(stats, path)
